e884a9f60b002ef27b7abb547dfb10012d38bbf0
Sarthak-Sthapit <sarthaksthapit0@gmail.com>
2025-07-30 18:22:04 +0545

    Inheritance example

---
+++ Animals/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class InheritanceExample
+{
+
+    class Animal
+    {
+        public string? Name;
+
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} is eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine($"{Name} is sleeping.");
+        }
+    }
+
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} says: Woof! ");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} says: Meow! ");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a name for the Dog: ");
+        Dog dog = new Dog();
+        dog.Name = Console.ReadLine();
+        dog.Eat();
+        dog.Bark();
+        dog.Sleep();
+
+        Console.WriteLine();
+
+        Console.Write("Enter a name for the Cat: ");
+        Cat cat = new Cat();
+        cat.Name = Console.ReadLine();
+        cat.Eat();
+        cat.Meow();
+        cat.Sleep();
+    }
+}
